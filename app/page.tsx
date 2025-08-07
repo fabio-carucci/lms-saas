e3498c709a04ce725b7342ae69b81e7eeef0d65b
Fabio Carucci <garu2000@live.it>
2025-08-07 09:39:31 +0200

    feat: companion card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,45 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
-    <div className="text-2xl underline">
-      <h1>Welcome to my LMS Saas App</h1>
-      <Button>Let&apos;s get started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#FFDA6E"
+        />
+        <CompanionCard
+          id="876"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={42}
+          color="#BDE7FF"
+        />
+        <CompanionCard
+          id="456"
+          name="Memo, the Memory Keeper"
+          topic="World Wars: Causes & Consequences"
+          subject="history"
+          duration={15}
+          color="#FFECC8"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
